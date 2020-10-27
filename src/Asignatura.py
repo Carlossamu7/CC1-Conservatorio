c1b191181d3427188228cd99c832ab7d208991a9
Carlos <carlossamu7@gmail.com>
2020-10-27 11:55:41 +0100

    Closes #12 Admin da de alta asignatura

---
+++ src/Asignatura.py
@@ -0,0 +1,40 @@
+class Asignatura:
+    def __init__(self, asignatura, profesor, horario, aula):
+        self.asignatura = asignatura
+        self.profesor = profesor
+        self.horario = horario
+        self.aula = aula
+
+    def getAsignatura(self):
+        return self.asignatura
+
+    def getProfesor(self):
+        return self.profesor
+
+    def getHorario(self):
+        return self.horario
+
+    def getAula(self):
+        return self.aula
+
+    def setAsignatura(self, asignatura):
+        self.asignatura = asignatura
+
+    def setProfesor(self, profesor):
+        self.profesor = profesor
+
+    def setHorario(self, horario):
+        self.horario = horario
+
+    def setAula(self, aula):
+        self.aula = aula
+
+    def printAsignatura(self):
+        print("--> " + self.asignatura)
+        print("Profesor: " + self.profesor)
+        print("Horario: " + self.horario)
+        print("Aula: " + self.aula)
+
+if __name__ == "__main__":
+    asig = Asignatura("LenguajeMusical", "JJ", "L:16-17, X:16-17", "Aula01")
+    asig.printAsignatura()
