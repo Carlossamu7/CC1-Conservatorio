d7e7973217bd12250ebc8e0b541506dea4879b4f
Carlos <carlossamu7@gmail.com>
2020-10-27 11:57:06 +0100

    Closes #15 Alumno matricula asignaturas

---
+++ src/Alumno.py
@@ -0,0 +1,38 @@
+class Alumno:
+    def __init__(self, nombre, email, dni, asignaturas):
+        self.nombre = nombre
+        self.email = email
+        self.dni = dni
+        self.asignaturas = asignaturas
+
+    def getNombre(self):
+        return self.nombre
+
+    def getEmail(self):
+        return self.email
+
+    def getDni(self):
+        return self.dni
+
+    def getAsignaturas(self):
+        return self.asignaturas
+
+    def setNombre(self, nombre):
+        self.nombre = nombre
+
+    def setEmail(self, email):
+        self.email = email
+
+    def setDni(self, dni):
+        self.dni = dni
+
+    def setAsignaturas(self, asignaturas):
+        self.asignaturas = asignaturas
+
+    def printAlumno(self):
+        print("--> " + self.nombre + " (DNI: " + self.dni + ", @: " + self.email + ")")
+        print("Asignaturas: " + self.asignaturas)
+
+if __name__ == "__main__":
+    alumno = Alumno("Carlos", "carlossamu7@correo.ugr.es", "00000000A", "LenguajeMusical")
+    alumno.printAlumno()
