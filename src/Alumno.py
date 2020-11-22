4a89a93850dd40699d3b0656bb437dff16a726a3
Carlos <carlossamu7@gmail.com>
2020-11-22 13:16:09 +0100

    #54 Uso de atributos privados en la clase Alumno

--- src/Alumno.py
+++ src/Alumno.py
@@ -28,58 +28,58 @@ def validoDNI(dni):
 
 class Alumno:
     def __init__(self, nombre, email, dni, asignaturas):
-        self.nombre = nombre
-        self.email = email
+        self.__nombre = nombre
+        self.__email = email
         if(validoDNI(dni)):
-            self.dni = dni
+            self.__dni = dni
         else: # Lanza excepción
             raise ValueError("El DNI no es válido.")
-        self.asignaturas = asignaturas
+        self.__asignaturas = asignaturas
 
     def getNombre(self):
-        return self.nombre
+        return self.__nombre
 
     def getEmail(self):
-        return self.email
+        return self.__email
 
     def getDni(self):
-        return self.dni
+        return self.__dni
 
     def getAsignaturas(self):
-        return self.asignaturas
+        return self.__asignaturas
 
     def setNombre(self, nombre):
-        self.nombre = nombre
+        self.__nombre = nombre
 
     def setEmail(self, email):
-        self.email = email
+        self.__email = email
 
     # El DNI no cambia, no existe setter.
 
     def setAsignaturas(self, asignaturas):
-        self.asignaturas = asignaturas
+        self.__asignaturas = asignaturas
 
     def listaAsignaturas(self):
-        return self.asignaturas.split(", ")
+        return self.__asignaturas.split(", ")
 
     def matriculaAsignatura(self, asig):
         if asig in self.listaAsignaturas():	# Lanza excepción
             raise ValueError("Asignatura ya matriculada.")
         else:
-            if self.asignaturas == "":
-                self.asignaturas = asig
+            if self.__asignaturas == "":
+                self.__asignaturas = asig
             else:
-                self.asignaturas += ", " + asig
+                self.__asignaturas += ", " + asig
 
     def desmatriculaAsignatura(self, asig):
         list = self.listaAsignaturas()
         if(asig in list):
             list.remove(asig)
-            self.asignaturas = ", ".join(list)
+            self.__asignaturas = ", ".join(list)
         else:	# Lanza excepción
             raise ValueError("No existe tal asignatura.")
 
     def toString(self):
-        str = "--> " + self.nombre + " (DNI: " + self.dni + ", @: " + self.email + ")" + "\n"
-        str += "    Asignaturas: " + self.asignaturas
+        str = "--> " + self.__nombre + " (DNI: " + self.__dni + ", @: " + self.__email + ")" + "\n"
+        str += "    Asignaturas: " + self.__asignaturas
         return str
