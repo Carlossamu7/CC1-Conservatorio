6776c02cb826e7820d93ff884784c63b2d6e2c8e
Carlos <carlossamu7@gmail.com>
2020-11-30 18:17:53 +0100

    #65 Creada la clase AsignaturaConcepto con métodos básicos (y tests correspondientes)

---
+++ tests/testAsignaturaConcepto.py
@@ -0,0 +1,37 @@
+"""
+Máster Universitario en Ingeniería Informática UGR
+Cloud Computing: Fundamentos e Infraestructuras (2020-2021)
+Carlos Santiago Sánchez Muñoz
+Implementación de tests sobre la clase AsignaturaConcepto
+"""
+
+import unittest
+import sys
+sys.path.append('src')
+
+from AsignaturaConcepto import AsignaturaConcepto
+
+class TestAsignaturaConcepto(unittest.TestCase):
+
+    def setUp(self):
+        self.asig = AsignaturaConcepto("001", "Lenguaje Musical", 1, "Nociones básicas acerca de leer una partitura, entonar y hacer dictados")
+
+    def test_get_id(self):
+        self.assertEqual(self.asig.get_id(), "001", "Comprobando get_id()")
+
+    def test_get_nombre_asignatura(self):
+        self.assertEqual(self.asig.get_nombre_asignatura(), "Lenguaje Musical", "Comprobando get_nombre_asignatura()")
+
+    def test_get_curso(self):
+        self.assertEqual(self.asig.get_curso(), 1, "Comprobando get_curso()")
+
+    def test_get_concepto(self):
+        self.assertEqual(self.asig.get_concepto(), "Nociones básicas acerca de leer una partitura, entonar y hacer dictados", "Comprobando get_concepto()")
+
+    def test_to_string(self):
+        self.assertEqual(self.asig.to_string(),
+            "--> Lenguaje Musical (001)\n    Curso: 1\n    Concepto: Nociones básicas acerca de leer una partitura, entonar y hacer dictados",
+            "Comprobando to_string()")
+
+if __name__ == '__main__':
+    unittest.main()
