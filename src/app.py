3544b38e9b808007de75778085af999dd02312c0
Carlossamu7 <carlossamu7@gmail.com>
2021-01-16 18:02:28 +0100

    #75 Pasando a función la lectura del json

--- src/app.py
+++ src/app.py
@@ -2,6 +2,23 @@ import os
 import json
 from Conservatorio import Conservatorio
 
+def lee_json():
+    try: # De https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file
+        if os.path.exists('../data/conservatorio.json'):
+            path='../data/conservatorio.json'
+        elif os.path.exists('./data/conservatorio.json'):
+            path='./data/conservatorio.json'
+        else:
+            raise IOError("No se encuentra 'conservatorio.json'")
+
+        with open(path) as data_file:
+            data = json.load(data_file)
+
+    except IOError as fallo:
+        print("Error {:s} leyendo conservatorio.json".format( fallo ) )
+
+    return data
+
 # Función para tener algunas instancias en el coleccionable
 def crea_conservatorio(data):
     # Creando el conservatorio
@@ -27,20 +44,8 @@ def crea_conservatorio(data):
     return conser
 
 if __name__ == '__main__':
-    try: # De https://stackoverflow.com/questions/2835559/parsing-values-from-a-json-file
-        if os.path.exists('../data/conservatorio.json'):
-            path='../data/conservatorio.json'
-        elif os.path.exists('./data/conservatorio.json'):
-            path='./data/conservatorio.json'
-        else:
-            raise IOError("No se encuentra 'conservatorio.json'")
-
-        with open(path) as data_file:
-            data = json.load(data_file)
-
-    except IOError as fallo:
-        print("Error {:s} leyendo conservatorio.json".format( fallo ) )
-
-    # Creamos el conservatorio con algunos alumnos y asignatura
+    # Leemos el fichero json
+    data = lee_json()
+    # Creamos el conservatorio con algunos alumnos y asignaturas
     conser = crea_conservatorio(data)
     print(conser.to_string())
